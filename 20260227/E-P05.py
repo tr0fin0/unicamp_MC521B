1a2e4c31d246ced44ececa18462b63624f9c917c
Guilherme Nunes Trofino <62114576+tr0fin0@users.noreply.github.com>
2026-03-06 13:52:44 -0300

    chore: termination of 20260227 activities (#1)

--- 20260227/E-P05.py
+++ 20260227/E-P05.py
@@ -4,23 +4,27 @@
 def main():
     pins : str = input()
 
-    columns = [
-        int(int(pins[6]) > 0),
-        int(int(pins[3]) > 0),
-        int((int(pins[1]) + int(pins[7])) > 0),
-        int((int(pins[0]) + int(pins[4])) > 0),
-        int((int(pins[2]) + int(pins[8])) > 0),
-        int(int(pins[5]) > 0),
-        int(int(pins[9]) > 0)
-    ]
-
     split = False
     if (int(pins[0]) == 0):
+        columns = [
+            int(int(pins[6]) > 0),
+            int(int(pins[3]) > 0),
+            int((int(pins[1]) + int(pins[7])) > 0),
+            int((int(pins[0]) + int(pins[4])) > 0),
+            int((int(pins[2]) + int(pins[8])) > 0),
+            int(int(pins[5]) > 0),
+            int(int(pins[9]) > 0)
+        ]
+
         for i in range(1, len(columns) - 1):
-            if columns[i] == 0:
-                
-                if sum(columns[0 : i]) > 0 and sum(columns[i+1:]):
-                    split = True
+            if (
+                columns[i] == 0 and
+                sum(columns[0 : i]) > 0 and
+                sum(columns[i+1: ]) > 0
+            ):
+                split = True
+
+                break
 
     print("Yes" if split else "No")
 
